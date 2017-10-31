2418abc21a204f88ec170f444b2a22570f1f8b0a
PJ <jardiolin.paolo@gmail.com>
2017-10-31 14:47:17 -0700

    changed file structure and cleaned up js

--- js/life-counter-terminal.js
+++ js/life-counter-terminal.js
@@ -9,9 +9,8 @@ let player = {
 }
 
 commandLine.onkeydown = function(e){
-    if(e.keyCode == 13){
+    if(e.keyCode == 13)
         submit();
-    }
 }
 
 function submit(){
@@ -22,33 +21,33 @@ function submit(){
         undoArr.push(player.life);
         modifyLife('life', command);
         displayStatus();
-    } else if(command == ''){
+    }else if(command == ''){
         // Do nothing.
-    } else if(command.match(/^reset$/i)){
+    }else if(command.match(/^reset$/i)){
         reset();
         displayStatus();
-    } else if(command.match(/^undo$/i)){
+    }else if(command.match(/^undo$/i)){
         undo();
         displayStatus();
-    } else if(command.match(/^cheer$/i)){
+    }else if(command.match(/^cheer$/i)){
         cheer();
-    } else if(command.match(/^color\s+\w+$/i)){
+    }else if(command.match(/^color\s+\w+$/i)){
         changeColor(command);
-    } else if(command.match(/^(inf|infect)\s+[\-\+\*\/\=]\d+$/i)){
+    }else if(command.match(/^(inf|infect)\s+[\-\+\*\/\=]\d+$/i)){
         modifyLife('infect', command);
         displayStatus();
-    } else if(command.match(/^(cmd|command)\s+(to|from)\s+\S+\s+[\-\+\*\/\=]\d+$/i)){
+    }else if(command.match(/^(cmd|command)\s+(to|from)\s+\S+\s+[\-\+\*\/\=]\d+$/i)){
         let customLife = command.match(/^(cmd|command)\s+(to|from)\s+(\S+)/i);
         customLife = customLife[3] + '(' + customLife[2] + '/cmd)';
         modifyLife(customLife, command);
         displayStatus();
-    } else if(command.match(/^roll\s+\d+$/i)){
+    }else if(command.match(/^roll\s+\d+$/i)){
         roll(command);
-    } else if(command.match(/^(show|sh)$/i)){
+    }else if(command.match(/^(show|sh)$/i)){
         displayStatus();
-    } else if(command.match(/^(delete|del)\s+\S+$/)){
+    }else if(command.match(/^(delete|del)\s+\S+$/)){
         deleteProperty(command);
-    } else {
+    }else{
         let invalidCommandMessage = "Command \'" + command + "\' is invalid.";
         displayCommand(invalidCommandMessage);
     }
@@ -58,17 +57,17 @@ function submit(){
 function deleteProperty(command){
     property = command.match(/\s+(\S+)$/)[1];
 
-    if(player.hasOwnProperty(property) && property != 'life'){
+    if(player.hasOwnProperty(property) && property != 'life')
         delete player[property];
-    } else if(property == 'life'){
+    else if(property == 'life')
         displayCommand("Property \'" + property + "\' cannot be deleted.");
-    } else {
+    else 
         displayCommand("Property \'" + property + "\' does not exist.");
-    }
 }
 
 function displayCommand(command){
     let item = document.createElement('DIV');
+
     item.innerHTML = command;
     history.appendChild(item);
     clear(commandLine);
@@ -78,9 +77,9 @@ function displayStatus(){
     let item = document.createElement('DIV');
     let status = '';
     
-    for(let prop in player){
+    for(let prop in player)
         status = status.concat(prop + ': ' + player[prop] + '<br>');
-    }
+
     item.innerHTML = status;
     history.appendChild(item);	
     clear(commandLine);	
@@ -90,9 +89,8 @@ function modifyLife(type, command){
     let operator = command.match(/[\-\+\*\/\=]/)[0];
     let operand = Number(command.match(/\d+$/)[0]);
     
-    if(!player.hasOwnProperty(type)){
+    if(!player.hasOwnProperty(type))
         player[type] = 0;
-    }
     
     switch(operator){
         case '-':
@@ -116,28 +114,25 @@ function modifyLife(type, command){
 function reset(){
     player.life = initialLife;
     for(let prop in player){
-        if(prop != 'life'){
+        if(prop != 'life')
             delete player[prop];
-        }
     }
     undoArr = [];
     divider = document.createElement('HR');
     history.appendChild(divider);
 }
 
-
 // Only affects life. Expand to affect custom life counts?
 function undo(){
-    console.log(undoArr);
-    if(undoArr.length == 0){
+    if(undoArr.length == 0)
         player.life = initialLife;
-    } else {
+    else
         player.life = undoArr.pop();
-    }
 }
 
 function cheer(){
-    let cheerAudio = new Audio('res/sound/honoka-faito-dayo.mp3');
+    let cheerAudio = new Audio('assets/sounds/honoka-faito-dayo.mp3');
+
     cheerAudio.play();
 }
 
@@ -148,6 +143,7 @@ function clear(ele){
 function changeColor(command){
     tried = command.match(/^color\s+(\w+)$/i)[1];
     color = command.match(/red|blue|yellow|white|pink|yellow|green|orange|teal/);
+
     if(color != null){
         color = color[0];
         document.getElementById('container').style.color = color;
